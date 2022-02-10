3780b7917944e5e2a32ee8fcd534f2a472cf4234
mattreed09 <matthew_reed@live.co.uk>
2022-02-10 14:14:27 +0000

    added second palette

--- R/edyn_colors.R
+++ R/edyn_colors.R
@@ -20,10 +20,13 @@
 #' @export
 
 
-edyn_colors <- function(n = 8, lightest = 0, inc_bw = F)
+edyn_colors <- function(n = 8, lightest = 0, inc_bw = F, palette = 2)
 {
   base_colors <- c('#C9D5C2','#F0EDE3','#E8CEC7','#FDF1BC','#CED5DD','#202A2C', '#000000','#FFFFFF')
+  colors2 <- c('#c9d5c2', '#EC9F05', '#9F4A54', '#262730', '#4F7292', '#509EA5',  '#000000','#FFFFFF' )
 
+  if(palette == 1)base_colors = base_colors
+  else base_colors = colors2
   m=floor((n-1)/6)
   if(inc_bw)
       return(c(unlist(purrr::map(0.8*((lightest/0.8):(m))/(m+1), ~colorspace::darken(base_colors[1:6], .x ) ))[1:(n - 2)], '#111111','#EEEEEE'))
